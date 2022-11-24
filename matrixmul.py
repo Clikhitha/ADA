62a3557f204330dea9651c0499be3e518940412c
Clikhitha <109733686+Clikhitha@users.noreply.github.com>
2022-11-24 16:54:53 +0530

    Add files via upload

---
+++ matrixmul.py
@@ -0,0 +1,16 @@
+x=[[12,3,7],
+   [4,5,6],
+   [7,8,9]]
+y=[[5,8,1,2],
+   [6,7,3,0],
+   [4,5,9,1]]
+result=[[0,0,0,0],
+        [0,0,0,0],
+        [0,0,0,0]]
+for i in range(len(x)):
+    for j in range(len(y[0])):
+        for k in range(len(y)):
+            result[i][j]+=x[i][k]*y[k][j]
+print('result of matrix multiplication is:')
+for r in result:
+    print(r)
